97e35862f35d8876e66085f3e4982155efce44db
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2018-01-22 21:53:07 -0500

    orm functions working

--- models/burger.js
+++ models/burger.js
@@ -1,13 +1,8 @@
 var orm = require("../config/orm.js");
 
-// // Find all the pets ordering by the lowest price to the highest price.
-// orm.selectAndOrder("animal_name", "pets", "price");
+orm.selectAll();
 
-// // Find a pet in the pets table by an animal_name of Rachel.
-// orm.selectWhere("pets", "animal_name", "Rachel");
+orm.insertOne("Chili Burger")
+orm.updateOne("Pastrami Burger");
 
-// // Find the buyer with the most pets.
-// orm.findWhoHasMost("buyer_name", "buyer_id", "buyers", "pets");
-
-
-module.exports = burgerFile;
+// module.exports = burgerFile;
